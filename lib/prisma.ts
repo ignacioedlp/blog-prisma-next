64b74c8b6cc3bde0a0ba4ff22137b8b7b8131188
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2022-01-12 12:13:05 -0300

    corregir errores v1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,15 +5,15 @@ declare global {
 
 import {PrismaClient} from '@prisma/client'; 
 
-let prisma: PrismaClient; //
+let prismaC: PrismaClient; //
 
 if(process.env.NODE_ENV === 'production'){
-  prisma = new PrismaClient();
+  prismaC = new PrismaClient();
 }
 else if(!global.prisma){
-  prisma = new PrismaClient();
+  prismaC = new PrismaClient();
 }else{
-  prisma = global.prisma;
+  prismaC = global.prisma;
 }
 
-export default prisma;
+export default prismaC;
